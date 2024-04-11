d9ed37aae30b9aa0d15453e440444b0983893810
Nikita Khon <n_khon@gamma.kz>
2024-04-11 09:51:25 +0500

    settings screen adjustments

--- app/src/main/java/com/example/florascope/CameraFragment.kt
+++ app/src/main/java/com/example/florascope/CameraFragment.kt
@@ -279,15 +279,3 @@ class CameraFragment : Fragment() {
         return byteBuffer
     }
 }
-
-
-
-//val conditions = CustomModelDownloadConditions.Builder()
-//    .requireWifi()
-//    .build()
-//FirebaseModelDownloader.getInstance()
-//.getModel("apple_firebase", DownloadType.LOCAL_MODEL, conditions)
-//.addOnCompleteListener {
-//    // Download complete. Depending on your app, you could enable the ML
-//    // feature, or switch from the local model to the remote model, etc.
-//}
--- app/src/main/java/com/example/florascope/SettingsFragment.kt
+++ app/src/main/java/com/example/florascope/SettingsFragment.kt
@@ -1,22 +1,13 @@
 package com.example.florascope
 
-import android.Manifest
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.content.Context
 import android.content.SharedPreferences
-import android.content.pm.PackageManager
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.florascope.databinding.FragmentSettingsBinding
@@ -38,12 +29,6 @@ class SettingsFragment : Fragment() {
 
         binding.feedbackBlock.setOnClickListener {
             view?.findNavController()?.navigate(R.id.action_settingsFragment_to_feedbackFragment)
-        }
-
-        binding.notificationSwitch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                sendNotification()
-            }
         }
 
         return binding.root
@@ -68,47 +53,6 @@ class SettingsFragment : Fragment() {
                 editor.putBoolean("night", true)
             }
             editor.apply()
-        }
-    }
-
-    private fun sendNotification() {
-        // Create a notification channel if running on Android Oreo or higher
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channelId = "default_channel_id"
-            val channelName = "Default Channel"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(channelId, channelName, importance).apply {
-                description = "Default channel for notifications"
-                enableLights(true)
-                lightColor = Color.RED
-            }
-            val notificationManager = requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-
-        // Create and show the notification
-        val notificationBuilder = NotificationCompat.Builder(requireContext(), "default_channel_id")
-            .setSmallIcon(R.drawable.ic_launcher_background)
-            .setContentTitle("Notification Title")
-            .setContentText("Notification content goes here")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-
-        with(NotificationManagerCompat.from(requireContext())) {
-            if (ActivityCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.POST_NOTIFICATIONS
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return
-            }
-            notify(1, notificationBuilder.build())
         }
     }
 }
