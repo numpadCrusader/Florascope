7115e14d518b2b587c8ee18a782211fbe0eb98e4
Nikita Khon <n_khon@gamma.kz>
2024-04-18 16:50:10 +0500

    language screen layout adjustments

--- app/src/main/java/com/example/florascope/LanguageSelectionFragment.kt
+++ app/src/main/java/com/example/florascope/LanguageSelectionFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.florascope.databinding.FragmentLanguageSelectionBinding
+import java.util.Locale
 
 class LanguageSelectionFragment : Fragment() {
     private lateinit var binding: FragmentLanguageSelectionBinding
@@ -16,6 +17,36 @@ class LanguageSelectionFragment : Fragment() {
     ): View {
         binding = FragmentLanguageSelectionBinding.inflate(inflater)
 
+        loadLang()
+
+        binding.languageEn.setOnClickListener {
+            binding.checkRu.visibility = View.GONE
+            binding.checkEng.visibility = View.VISIBLE
+        }
+
+        binding.languageRu.setOnClickListener {
+            binding.checkRu.visibility = View.VISIBLE
+            binding.checkEng.visibility = View.GONE
+        }
+
         return binding.root
+    }
+
+    private fun loadLang() {
+        if (isAppLanguageEnglish()) {
+            binding.checkRu.visibility = View.GONE
+            binding.checkEng.visibility = View.VISIBLE
+        } else {
+            binding.checkRu.visibility = View.VISIBLE
+            binding.checkEng.visibility = View.GONE
+        }
+    }
+
+    private fun isAppLanguageEnglish(): Boolean {
+        // Get the current locale of the app
+        val currentLocale = Locale.getDefault()
+
+        // Check if the language of the app is English
+        return currentLocale.language == "en"
     }
 }
