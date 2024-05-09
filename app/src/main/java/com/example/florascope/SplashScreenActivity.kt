c2c8f1b86dbbf3b8952ae2e136f436480be61900
numpadCrusader <200107044@stu.sdu.edu.kz>
2024-05-09 21:54:36 +0500

    ui adjustments

--- app/src/main/java/com/example/florascope/SplashScreenActivity.kt
+++ app/src/main/java/com/example/florascope/SplashScreenActivity.kt
@@ -1,9 +1,13 @@
 package com.example.florascope
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.graphics.drawable.DrawableCompat.applyTheme
 import com.example.florascope.fragments.MainActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -12,17 +16,30 @@ import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var sharedPreferences: SharedPreferences
     private val splashTimeOut: Long = 2000 // Splash screen timeout in milliseconds
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        sharedPreferences = getSharedPreferences("Mode", Context.MODE_PRIVATE)
+        applyTheme()
+
         // Start a coroutine to delay the transition to the main activity
         CoroutineScope(Dispatchers.Main).launch {
             delay(splashTimeOut)
             startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
             finish()
+        }
+    }
+
+    private fun applyTheme() {
+        val nightMode = sharedPreferences.getBoolean("night", false)
+        if (nightMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
     }
 }
